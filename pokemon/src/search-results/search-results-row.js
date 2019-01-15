6e972835b458c19945733e131cbbd0887c8129f9
Stefan Isaksson <stefan.foo.isaksson@gmail.com>
2019-01-15 23:47:04 +0100

    Displaying more fields on search result items

--- pokemon/src/search-results/search-results-row.js
+++ pokemon/src/search-results/search-results-row.js
@@ -11,10 +11,14 @@ const SearchResultsRow = (props) => {
             <h5>{props.pokemon.pokedex_number}</h5>
         </td>
         <td>
-            <h3>{props.pokemon.name}</h3>
+            <h3>{props.pokemon.name}</h3> 
+            {props.pokemon.is_legendary === 1 ? <h5>(legandary)</h5> : ""}
         </td>
         <td>
-            <img className="small_picture" src={`https://www.serebii.net/sunmoon/pokemon/${('000' + props.pokemon.pokedex_number).slice(-3)}.png`} alt="" ></img>
+            <img src={`https://www.serebii.net/sunmoon/pokemon/${('000' + props.pokemon.pokedex_number).slice(-3)}.png`} alt="" ></img>
+        </td>
+        <td>
+            <h5>{props.pokemon.type1} {props.pokemon.type2}</h5>
         </td>
     </tr>
 };
